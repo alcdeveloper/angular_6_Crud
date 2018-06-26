7ed2220ebbf2b3282bd43dfa1afd2306d8a7d223
alcdeveloper85 <alcdeveloper85@gmail.com>
2018-06-26 11:06:16 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
+
+
+import { APP_ROUTING } from './app.routes';
+
+
+import { HeroesService } from './services/heroes.service'; 
+
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesEditComponent } from './components/heroes-edit/heroes-edit.component';
+import { KeysPipe } from './pipes/keys.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroesEditComponent,
+    KeysPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
